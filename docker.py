9fd59b03bae73b7282f8f8115c21d8ba2c23d9da
Martin Kåberg <martin.kaberg@nordcloud.se>
2016-12-12 12:30:38 +0100

    moved things around

--- docker.py
+++ docker.py
@@ -3,12 +3,10 @@ import yaml, os
 
 
 class Compose2TaskDefinition():
-    def __init__(self, compose_file):
+    def __init__(self, compose_file, name_image_map):
         self.compose_file = compose_file
-
-    def get_task_definition(self, title, name_image_map):
-
-        filename = os.path.join(os.path.dirname(__file__), '../docker-compose.yml')
+        self.container_definitions = {}
+        filename = os.path.join(os.path.dirname(__file__), self.compose_file)
         f = open(filename)
         data_map = yaml.safe_load(f)
         f.close()
@@ -27,20 +25,29 @@ class Compose2TaskDefinition():
 
             if data_map[name].get("links") is not None:
                 links = []
+            self.container_definitions[name] = ecs.ContainerDefinition(
+                Name=name,
+                Image=name_image_map[name],
+                Cpu=data_map[name].get("cpu_shares", Ref("AWS::NoValue")),
+                # TODO Convert correct with M and G
+                Memory=data_map[name]["mem_limit"][:-1],
+                Essential=True,
+                PortMappings=port_mappings,
+                Links=data_map[name].get("links", Ref("AWS::NoValue"))
 
-            defs.append(
-                ecs.ContainerDefinition(
-                    Name=name,
-                    Image=name_image_map[name],
-                    Cpu=data_map[name].get("cpu_shares", Ref("AWS::NoValue")),
-                    # TODO Convert correct with M and G
-                    Memory=data_map[name]["mem_limit"][:-1],
-                    Essential=True,
-                    PortMappings=port_mappings,
-                    Links=data_map[name].get("links", Ref("AWS::NoValue"))
-
-                )
             )
+            defs.append(
+                self.container_definitions[name]
+            )
+
+    def get_container_definition(self, name):
+        return self.container_definitions[name]
+
+    def get_task_definition(self, title):
+
+        defs = []
+        for name in self.container_definitions:
+            defs.append(self.container_definitions[name])
         return ecs.TaskDefinition(
             title,
             ContainerDefinitions=defs
